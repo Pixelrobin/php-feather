7c770097f6b0a7396291b17f5d86f3b86940a724
Michael Savchuk <pixelrobin@hotmail.com>
2018-10-27 15:55:34 -0700

    Initial commit

---
+++ bin/build.js
@@ -0,0 +1,29 @@
+const feather = require('feather-icons');
+const fs      = require('fs');
+const path    = require('path');
+
+const outPath = path.resolve(__dirname, '../icons');
+let defaultAttrs = null;
+
+Object.keys(feather.icons)
+	.forEach(key => {
+		const { name, contents, attrs } = feather.icons[key];
+		
+		if (defaultAttrs === null) {
+			defaultAttrs = attrs;
+		}
+
+		fs.writeFileSync(path.resolve(outPath, name + '.svg'), contents);
+	});
+
+delete defaultAttrs.class;
+
+let php = Object.keys(defaultAttrs)
+	.map(key => `'${ key }' => '${ defaultAttrs[key] }'`)
+	.reduce((final, current) => final + current + ',', '');
+
+php = '<?php namespace Feather; const DEFAULT_ATTRS = array('
+	+ php.substring(0, php.length - 1)
+	+ ');';
+
+fs.writeFileSync(path.resolve(__dirname, '../src/defaultAttributes.php'), php);
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+include 'src/Feather.php';
+
+\Feather\Icon('anchor');
