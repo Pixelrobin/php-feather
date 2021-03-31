34f38517649e833fd5675ccbe2eb1ac2edac8fea
Nate Wiebe <nate@northern.co>
2021-03-31 19:40:58 -0400

    Format files

--- tests/FeatherTest.php
+++ tests/FeatherTest.php
@@ -2,61 +2,67 @@
 
 use PHPUnit\Framework\TestCase;
 
-class FeatherTest extends TestCase {
-	protected function setUp() {
-		$this->icons = new Feather\Icons;
+class FeatherTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->icons = new Feather\Icons();
 
-		// Data from JS
+        // Data from JS
 
-		$this->XMLTestData = json_decode(
-			file_get_contents(__DIR__ . '/XMLTestData.json'),
-			true
-		);
+        $this->XMLTestData = json_decode(
+            file_get_contents(__DIR__ . '/XMLTestData.json'),
+            true
+        );
 
-		$this->AttributeTestData = json_decode(
-			file_get_contents(__DIR__ . '/AttributeTestData.json'),
-			true
-		);
-	}
+        $this->AttributeTestData = json_decode(
+            file_get_contents(__DIR__ . '/AttributeTestData.json'),
+            true
+        );
+    }
 
-	public function testIconsHasDefaultAttributes() {
-		$this->assertEquals(Feather\DEFAULT_ATTRIBUTES, $this->icons->getAttributes());
-	}
+    public function testIconsHasDefaultAttributes()
+    {
+        $this->assertEquals(Feather\DEFAULT_ATTRIBUTES, $this->icons->getAttributes());
+    }
 
-	public function testIconDefaultXML() {
-		foreach ($this->XMLTestData as $test_data) {
-			$this->assertXMLStringEqualsXMLString(
-				$test_data['xml'],
-				$this->icons->get($test_data['name'], array(), false),
-				'Icon fail: ' . $test_data['name']
-			);
-		}
-	}
+    public function testIconDefaultXML()
+    {
+        foreach ($this->XMLTestData as $test_data) {
+            $this->assertXMLStringEqualsXMLString(
+                $test_data['xml'],
+                $this->icons->get($test_data['name'], [], false),
+                'Icon fail: ' . $test_data['name']
+            );
+        }
+    }
 
-	public function testIconXMLWithAttributes() {
-		$test_data = $this->AttributeTestData;
+    public function testIconXMLWithAttributes()
+    {
+        $test_data = $this->AttributeTestData;
 
-		$icon = $this->icons->get($test_data['name'], $test_data['attributes'], false);
+        $icon = $this->icons->get($test_data['name'], $test_data['attributes'], false);
 
-		$this->assertXMLStringEqualsXMLString(
-			$test_data['xml'],
-			$icon,
-			'XML custom attribute fail'
-		);
-	}
+        $this->assertXMLStringEqualsXMLString(
+            $test_data['xml'],
+            $icon,
+            'XML custom attribute fail'
+        );
+    }
 
-	public function testIconXMLWithAttributesFromClass() {
-		$test_data = $this->AttributeTestData;
+    public function testIconXMLWithAttributesFromClass()
+    {
+        $test_data = $this->AttributeTestData;
 
-		$this->icons->setattributes($test_data['attributes']);
-		$icon = $this->icons->get($test_data['name'], array(), false);
+        $this->icons->setattributes($test_data['attributes']);
+        $icon = $this->icons->get($test_data['name'], [], false);
 
-		$this->assertXMLStringEqualsXMLString(
-			$test_data['xml'],
-			$icon,
-			'XML custom attribute on class fail'
-		);
+        $this->assertXMLStringEqualsXMLString(
+            $test_data['xml'],
+            $icon,
+            'XML custom attribute on class fail'
+        );
 
-		$this->icons->setAttributes(Feather\DEFAULT_ATTRIBUTES);
-	}
+        $this->icons->setAttributes(Feather\DEFAULT_ATTRIBUTES);
+    }
 }
