fa3858aaddfc7fe385776862f4727c604351d096
Nate Wiebe <nate@northern.co>
2021-04-05 14:20:30 -0400

    Create attributes file with php build script

--- bin/build.php
+++ bin/build.php
@@ -1,7 +1,23 @@
 <?php
 
+function getSvgAttributes(string $svg): array
+{
+    $attributes = [];
 
+    $icon = new SimpleXMLElement($svg);
 
+    $xmlNamespaces = $icon->getDocNamespaces();
+
+    $attributes['xmlns'] = array_shift($xmlNamespaces);
+
+    foreach ($icon->attributes() as $attribute => $value) {
+        $attributes[(string)$attribute] = (string)$value;
+    }
+
+    unset($attributes['class']);
+
+    return $attributes;
+}
 
 function getSvgContents(string $svg): string
 {
@@ -23,6 +39,25 @@ function getSvgContents(string $svg): string
     return $svgContents;
 }
 
+function writeAttributes(array $attributes): void
+{
+    global $rootDir;
+
+    $export = var_export($attributes, true);
+
+    file_put_contents(
+        implode(DIRECTORY_SEPARATOR, [$rootDir, 'src', 'defaultAttributes.php']),
+        <<<EOT
+    <?php
+
+    /* !!! THIS FILE IS AUTO-GENERATED !!! */
+
+    namespace Feather;
+
+    const DEFAULT_ATTRIBUTES = $export;
+
+    EOT
+    );
 }
 
 function writeIcons(array $icons): void
@@ -46,10 +81,17 @@ $rootDir = implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), '..']);
 
 $icons = [];
 
+$defaultAttributes = null;
+
 foreach (glob(implode(DIRECTORY_SEPARATOR, [$rootDir, 'vendor', 'npm-asset', 'feather-icons', 'dist', 'icons', '*.svg'])) as $file) {
     $iconName = preg_replace('/\.svg$/i', '', basename($file));
+
+    if (null === $defaultAttributes) {
+        $defaultAttributes = getSvgAttributes(file_get_contents($file));
+    }
 
     $icons[$iconName] = getSvgContents(file_get_contents($file));
 }
 
+writeAttributes($defaultAttributes);
 writeIcons($icons);
