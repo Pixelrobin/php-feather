f7e3081ed0568a784a37026632c9088648cdba38
Nate Wiebe <nate@northern.co>
2021-04-09 13:07:07 -0400

    Add helper functions

--- src/SvgAttributesTrait.php
+++ src/SvgAttributesTrait.php
@@ -31,4 +31,59 @@ trait SvgAttributesTrait
     {
         return $this->attributes;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getAttribute(string $name)
+    {
+        return $this->attributes[$name] ?? null;
+    }
+
+    public function setSize(float $width, ?float $height = null): self
+    {
+        $this->setAttribute('width', $width)
+             ->setAttribute('height', $height ?? $width);
+
+        return $this;
+    }
+
+    public function getSize(): array
+    {
+        return ['width' => $this->getAttribute('width'), 'height' => $this->getAttribute('height')];
+    }
+
+    public function getWidth(): float
+    {
+        return (float)$this->getAttribute('width');
+    }
+
+    public function getHeight(): float
+    {
+        return (float)$this->getAttribute('height');
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->setAttribute('stroke', $color);
+
+        return $this;
+    }
+
+    public function getColor(): string
+    {
+        return (string)$this->getAttribute('stroke');
+    }
+
+    public function setWeight(float $weight): self
+    {
+        $this->setAttribute('stroke-width', $weight);
+
+        return $this;
+    }
+
+    public function getWeight(): float
+    {
+        return (float)$this->getAttribute('stroke-width');
+    }
 }
